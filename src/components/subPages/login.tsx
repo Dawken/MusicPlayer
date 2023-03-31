b5369614f5beddaadf72106746c2ce9b9f68d513
Dawid Domiński <dawid.dominski1125@gmail.com>
2023-03-31 02:33:30 +0200

    feat: add login api call to Login component

--- src/components/subPages/login.tsx
+++ src/components/subPages/login.tsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import {spotifyAuthenticationUrl} from '../shared/spotifyAuthenticationUrl'
-
+import React, { useEffect } from 'react';
+import { spotifyAuthenticationUrl } from '../../shared/spotifyAuthenticationUrl';
+import { useMutation } from 'react-query';
+import { store, useAppSelector } from '../../redux/store';
+import { getClientResponse } from '../../redux/user';
+import musicPlayerBackend from '../../config/axiosConfig';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-	return (
-		<a href={spotifyAuthenticationUrl.url}>Login</a>
-	)
-}
-export default Login
+	const code = localStorage.getItem('code');
+	const isLogged = useAppSelector((state) => state.auth.isLoggedIn);
+	const navigate = useNavigate();
+
+	const { isLoading, mutate: login } = useMutation(
+		() => {
+			return musicPlayerBackend.post('/api/login', {
+				code,
+			});
+		},
+		{
+			onSuccess: () => {
+				store.dispatch(getClientResponse({ isLogged: true }));
+				window.history.pushState({}, '', '/');
+				navigate('/');
+			},
+			onError: () => {
+				navigate('/login');
+			},
+		}
+	);
+	useEffect(() => {
+		if (code && !isLogged) {
+			login();
+		}
+	}, []);
+
+	if (isLoading) return <div>Loading...</div>;
+	return <a href={spotifyAuthenticationUrl.url}>Login</a>;
+};
+export default Login;
