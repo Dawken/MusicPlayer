04a892abc2ccf3f56ab364fb8b4124b5a8ef0a35
Dawid Domiński <dawid.dominski1125@gmail.com>
2023-03-31 02:31:09 +0200

    feat: manage private routes with redux state, change eslintrc config

--- src/components/utils/privateRoutes.tsx
+++ src/components/utils/privateRoutes.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Login from '../subPages/login';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAppSelector } from '../../redux/store';
 
 const code = new URLSearchParams(window.location.search).get('code');
-
+if (code) localStorage.setItem('code', code);
 const PrivateRoutes = () => {
-	return code ? <Outlet context={code} /> : <Login />;
+	const isLogged = useAppSelector((state) => state.auth.isLoggedIn);
+	return isLogged ? <Outlet /> : <Navigate to={'/login'} />;
 };
 
 export default PrivateRoutes;
