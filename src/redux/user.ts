d82fedfd52a55a1e0fc7b12999909c5bf8d8a1c4
Dawid Domiński <dawid.dominski1125@gmail.com>
2023-03-31 02:23:20 +0200

    feat: add redux isLogged state, move Auth url to src

---
+++ src/redux/user.ts
@@ -0,0 +1,22 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export type StatusCode = {
+	isLoggedIn: boolean;
+};
+
+export const initialState: StatusCode = { isLoggedIn: false };
+
+export const user = createSlice({
+	name: 'clientResponse',
+	initialState,
+	reducers: {
+		getClientResponse: (
+			state,
+			action: PayloadAction<{ isLogged: boolean }>
+		) => {
+			state.isLoggedIn = action.payload.isLogged;
+		},
+	},
+});
+
+export const { getClientResponse } = user.actions;
