c9530efb816771753122adfb897dfe03db8b0124
Dawid Domiński <dawid.dominski1125@gmail.com>
2023-03-31 02:36:12 +0200

    feat: add searching tracks functionality to layout

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,10 +1,42 @@
-import React from 'react';
-import { useOutletContext } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 import useAuth from '../../customHooks/useAuth';
+import SpotifyWebApi from 'spotify-web-api-node';
+
+const spotifyApi = new SpotifyWebApi({
+	clientId: process.env.REACT_APP_SPOTIFY_API,
+});
 
 const Layout = () => {
-	const code = useOutletContext();
-	const accessToken = useAuth();
-	return <>{code}</>;
+	const spotify = useAuth();
+	const { accessToken } = spotify;
+
+	const [search, setSearch] = useState('');
+	const [searchResult, setSearchResult] = useState<
+		SpotifyApi.TrackObjectSimplified[]
+	>([]);
+
+	useEffect(() => {
+		if (accessToken) {
+			spotifyApi.setAccessToken(accessToken);
+		}
+	}, [accessToken]);
+
+	useEffect(() => {
+		if (!search) return setSearchResult([]);
+		if (!accessToken) return;
+
+		spotifyApi.searchTracks(search).then((res) => {
+			if (res.body.tracks) {
+				setSearchResult(res.body.tracks.items);
+			}
+		});
+	}, [search]);
+	console.log(searchResult);
+	return (
+		<input
+			onChange={(event) => setSearch(event.target.value)}
+			value={search}
+		></input>
+	);
 };
 export default Layout;
