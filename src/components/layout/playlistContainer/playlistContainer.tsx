c5d041c61f45af5fad59f1ae7b59e2cce0428753
Dawid Domiński <dawid.dominski1125@gmail.com>
2023-04-05 20:51:00 +0200

    feat: display quantity songs based on device width, improve RWD

--- src/components/layout/playlistContainer/playlistContainer.tsx
+++ src/components/layout/playlistContainer/playlistContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import TrackSearchResult from './trackSearchResult/trackSearchResult'
 import { Track } from '../../../types/searchTracksResponse'
 import usePlaylistContainer from './usePlaylistContainer'
@@ -11,15 +11,55 @@ type SearchResultType = {
 const PlaylistContainer = ({ searchResult }: SearchResultType) => {
 	const { tracks } = usePlaylistContainer()
 
+	const [visibleItems, setVisibleItems] = useState(6)
+
+	useEffect(() => {
+		const handleResize = () => {
+			const windowWidth = window.innerWidth
+			switch (true) {
+				case windowWidth <= 1630 && windowWidth >= 1360:
+					setVisibleItems(5)
+					break
+				case windowWidth <= 1360 && windowWidth >= 1090:
+					setVisibleItems(4)
+					break
+				case windowWidth < 1090 && windowWidth >= 810:
+					setVisibleItems(3)
+					break
+				case windowWidth < 810:
+					setVisibleItems(2)
+					break
+				default:
+					setVisibleItems(6)
+			}
+		}
+		handleResize()
+		window.addEventListener('resize', handleResize)
+		return () => {
+			window.removeEventListener('resize', handleResize)
+		}
+	}, [])
+
 	return (
 		<div className={styles.mainContainer}>
-			{searchResult.length > 0
-				? searchResult.map((item) => {
-						return <TrackSearchResult item={item} key={item.id} />
-				  })
-				: tracks.map((item) => {
+			{searchResult.length > 0 && (
+				<section className={styles.bestResults}>
+					<div className={styles.sectionText}>Best results</div>
+					<div className={styles.bestResultsTracks}>
+						{searchResult.slice(0, visibleItems).map((item) => {
+							return <TrackSearchResult item={item} key={item.id} />
+						})}
+					</div>
+				</section>
+			)}
+			<section className={styles.lastPlayed}>
+				<div className={styles.sectionText}>Last Played</div>
+				<div className={styles.lastPlayedTracks}>
+					{tracks.slice(0, visibleItems).map((item) => {
 						return <TrackSearchResult item={item.track} key={item.track.id} />
-				  })}
+					})}
+				</div>
+			</section>
 		</div>
 	)
 }
