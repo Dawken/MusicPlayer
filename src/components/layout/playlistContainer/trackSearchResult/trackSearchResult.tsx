4235551807bb7c9806ba60647e50f4c7b2f1b2db
Dawid Domiński <dawid.dominski1125@gmail.com>
2023-04-09 00:54:51 +0200

    feat: add items to sidebarMenu, fix RWD

--- src/components/layout/playlistContainer/trackSearchResult/trackSearchResult.tsx
+++ src/components/layout/playlistContainer/trackSearchResult/trackSearchResult.tsx
@@ -18,12 +18,12 @@ const TrackSearchResult = ({ item }: TrackSearchResultProps) => {
 	return (
 		<div className={styles.songContainer} onClick={() => setSong()}>
 			<div className={styles.songPhotoContainer}>
-				<img className={styles.songPhoto} src={item.album.images[1].url} />
+				<img className={styles.songPhoto} src={item.album.images[1]?.url} />
 				<PlayCircleFilledWhiteIcon className={styles.playIcon} />
 			</div>
-			<div className={styles.artistName}>{item.artists[0].name}</div>
+			<div className={styles.artistName}>{item.artists[0]?.name}</div>
 			<div className={styles.songName}>
-				{item.name.length > 40 ? item.name.slice(0, 40) + '...' : item.name}
+				{item.name.length > 35 ? item.name.slice(0, 35) + '...' : item.name}
 			</div>
 		</div>
 	)
--- src/components/sharedComponents/sidebarMenu/sidebarMenu.tsx
+++ src/components/sharedComponents/sidebarMenu/sidebarMenu.tsx
@@ -1,10 +1,57 @@
 import React from 'react'
 import styles from './sidebarMenu.module.scss'
+import SpotifyLogo from '../../../assets/spotify.png'
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined'
+import HomeIcon from '@mui/icons-material/Home'
+import CollectionsBookmarkIcon from '@mui/icons-material/CollectionsBookmark'
+import CollectionsBookmarkOutlinedIcon from '@mui/icons-material/CollectionsBookmarkOutlined'
+import LibraryAddSharpIcon from '@mui/icons-material/LibraryAddSharp'
+import LibraryAddOutlinedIcon from '@mui/icons-material/LibraryAddOutlined'
+import { useLocation } from 'react-router-dom'
 
 const SidebarMenu = () => {
+	const params = useLocation()
+
+	const { pathname } = params
+
 	return (
 		<nav className={styles.sidebarMenuContainer}>
-			<div />
+			<div className={styles.homePage}>
+				<img src={SpotifyLogo} className={styles.spotifyLogo} />
+				<div className={styles.spotifyText}>Spotify</div>
+			</div>
+			<ul className={styles.optionsList}>
+				<li>
+					<div className={styles.listOption}>
+						{pathname === '/' ? (
+							<HomeIcon className={styles.icon} />
+						) : (
+							<HomeOutlinedIcon className={styles.icon} />
+						)}
+						<div className={styles.listOptionText}>Home</div>
+					</div>
+				</li>
+				<li>
+					<div className={styles.listOption}>
+						{pathname === '/createplaylist' ? (
+							<LibraryAddSharpIcon className={styles.icon} />
+						) : (
+							<LibraryAddOutlinedIcon className={styles.icon} />
+						)}
+						<div className={styles.listOptionText}>Create playlist</div>
+					</div>
+				</li>
+				<li>
+					<div className={styles.listOption}>
+						{pathname === '/playlists' ? (
+							<CollectionsBookmarkIcon className={styles.icon} />
+						) : (
+							<CollectionsBookmarkOutlinedIcon className={styles.icon} />
+						)}
+						<div className={styles.listOptionText}>My playlists</div>
+					</div>
+				</li>
+			</ul>
 		</nav>
 	)
 }
