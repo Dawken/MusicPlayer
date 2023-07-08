39706d07df57feb3fcda3be789da09433c25e6aa
Dawid Domiński <dawid.dominski1125@gmail.com>
2023-07-08 18:19:11 +0200

    feat: update login subpage design

--- src/components/subPages/login/login.tsx
+++ src/components/subPages/login/login.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import { spotifyAuthenticationUrl } from '../../../shared/spotifyAuthenticationUrl'
 import useLogin from './useLogin'
+import styles from './login.module.scss'
+import SpotifyLogo from '../../../assets/spotify.png'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
 	const { isLoading } = useLogin()
 
 	if (isLoading) return <div>Loading...</div>
-	return <a href={spotifyAuthenticationUrl.url}>Login</a>
+	return (
+		<div className={styles.layout}>
+			<div className={styles.spotify}>
+				<img src={SpotifyLogo} className={styles.spotifyLogo} />
+				<div className={styles.spotifyText}>Spotify</div>
+			</div>
+			<Link
+				to={spotifyAuthenticationUrl.url}
+				className={styles.loginButton}
+			>
+				<span>Login</span>
+			</Link>
+		</div>
+	)
 }
 export default Login
