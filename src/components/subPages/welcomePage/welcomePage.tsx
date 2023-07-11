69acc12ea4f4f1d48e74547a48e3cd0b0e4f2d15
Dawid Domiński <dawid.dominski1125@gmail.com>
2023-07-11 15:38:34 +0200

    feat: add login button to welcome page

--- src/components/subPages/welcomePage/welcomePage.tsx
+++ src/components/subPages/welcomePage/welcomePage.tsx
@@ -3,12 +3,16 @@ import threejs from './threejs'
 import styles from './welcomePage.module.scss'
 import 'animate.css'
 import { FullPage, Slide } from 'react-full-page'
+import { Link } from 'react-router-dom'
 
 const WelcomePage = () => {
 	const canvasRef = threejs()
 
 	return (
 		<div className={styles.layoutBackground}>
+			<Link to={'/login'} className={styles.login}>
+				<span>Login</span>
+			</Link>
 			<FullPage>
 				<Slide>
 					<div className={styles.headerTitle}>
